fdf663450632306c8971fbb856b2cb441e44cae4
Alain Plattner <alainplattner@gmail.com>
2019-01-06 20:20:15 -0600

    added option for spline interpolation

--- gprpy/interpSurface.py
+++ gprpy/interpSurface.py
@@ -3,8 +3,9 @@ import numpy as np
 from pyevtk.hl import gridToVTK
 
 
-def interpSurface(pointfile,outfile,nxgrid=100,nygrid=100,method='nearest',delimiter='\t'):
-    # method could be 'nearest' or 'cubic'. To extrapolate: use 'nearest'
+def interpSurface(pointfile,outfile,nxgrid=100,nygrid=100,method='spline',delimiter='\t',kx=1,ky=1):
+    # method could be 'nearest', 'linear', 'cubic', or 'spline'.
+    # For spline: Set kx and ky. They are the orders of the polynomial
     
     # Read in the point values
     pointdata=np.loadtxt(pointfile,delimiter=delimiter)
@@ -22,14 +23,20 @@ def interpSurface(pointfile,outfile,nxgrid=100,nygrid=100,method='nearest',delim
     X,Y=np.meshgrid(x,y)        
     
     # Interpolate the point values
-    Z=interp.griddata(points,values,(X,Y),method=method)#'nearest')#'cubic')
+    if method == 'spline':
+        spl = interp.SmoothBivariateSpline(points[:,0],points[:,1],
+                                           values,kx=kx,ky=ky)
+        Z = spl.ev(X,Y)
+    else:
+        Z=interp.griddata(points,values,(X,Y),method=method)
+        
     colval=np.zeros(Z.shape)
     
     # Write out vts file
     XX = np.tile(X,(1,1,1))
     YY = np.tile(Y,(1,1,1))
     ZZ = np.tile(Z,(1,1,1))  
-    gridToVTK(outfile,XX,YY,ZZ)#,cellData={'surface': colval})
+    gridToVTK(outfile,XX,YY,ZZ)
 
 
 
