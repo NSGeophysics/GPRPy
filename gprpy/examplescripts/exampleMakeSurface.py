cd105f5175c78d6913c2cbd92065f1a0e4c01022
Alain Plattner <alainplattner@gmail.com>
2019-01-24 09:25:31 -0600

    added spline example to surface interpolation

--- gprpy/examplescripts/exampleMakeSurface.py
+++ gprpy/examplescripts/exampleMakeSurface.py
@@ -3,6 +3,9 @@ from gprpy.interpSurface import *
 xsamples=200
 ysamples=200
 
+# I usually like spline best. It is smooth and extrapolates
+interpSurface('../exampledata/pickedSurfaceData/testpick_3D.txt','exampleSurf_skx1ky2',xsamples,ysamples,'spline',kx=1,ky=2)
+
 # 'nearest' extrapolates but makes jumpy surface 
 interpSurface('../exampledata/pickedSurfaceData/testpick_3D.txt','exampleSurf_n',xsamples,ysamples,'nearest')
 
