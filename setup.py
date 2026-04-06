c787e63c24cc3482863c80ff24c93a04a9f2eee6
Alain Plattner <alainplattner@gmail.com>
2026-04-06 09:47:49 -0500

    added 'import setuptools' to the setup function. Should now work with newer Python versions

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()    
     
 setuptools.setup(
     name="gprpy",
